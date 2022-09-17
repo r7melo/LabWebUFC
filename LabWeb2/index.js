d0f8c1e77aa37ef87a332c9fb4e35aa8233a7e58
r7melo <romariomelo360@gmail.com>
2022-09-16 21:56:47 -0300

    correcao dos labs 1 e 2

---
+++ LabWeb2/index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static('public'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/mirror', function (req, res, next) {
+    console.log(req.body)
+    res.send(req.body)
+})
+
+app.get('/querytojson', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})  
+
+app.get('/paramtojson/name/:name/role/:role', (req, res) => {
+    console.log(req.params);
+    res.send(req.params);
+})
+
+app.get('/animal', (req, res) => {
+
+    zoo = [
+        {type:"DOG", name:"Pluto"},
+        {type:"CAT", name:"Hercules"},
+        {type:"BIRD", name:"Tweety"},
+        {type:"DOG", name:"Spiff"},
+        {type:"CAT", name:"Tom"},
+        {type:"BIRD", name:"Road Runner"} 
+    ]
+
+    res_zoo = []
+
+    if(req.query.name){
+        zoo.forEach(animal => {
+            if(animal.name.toLowerCase().includes(req.query.name.toLowerCase()))
+                res_zoo.push(animal)
+        })
+    }
+    else if(req.query.animal){
+        zoo.forEach(animal => {
+            if(animal.type == req.query.animal)
+                res_zoo.push(animal)
+        })
+    }
+
+    console.log(res_zoo)
+    res.send(res_zoo)
+})
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
