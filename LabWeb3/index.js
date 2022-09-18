079dd3cb09fcde4a38ac8baab81e1f76ead28dd4
r7melo <romariomelo360@gmail.com>
2022-09-17 23:51:54 -0300

    Praticando NodeEJS

--- LabWeb3/index.js
+++ LabWeb3/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
+app.set('view engine', 'ejs')
+app.set('views', './views')
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,12 +12,20 @@ var CATEGORIAS = [
 
 
 app.get('/categorias', (req, res) => {
-    res.render('categorias', {categorias: CATEGORIAS});
-});
+    res.render('categorias', {categorias: CATEGORIAS})
+})
   
+app.get('/categoria-deletar/:id', (req, res) => {
+    if(req.params.id){
+        CATEGORIAS.forEach(categoria => {
+            if(categoria.id == req.params.id)
+                delete(categoria)
+                console.log(`${categoria} deletado`)
+        })
+    }
+})
 
 
-
-app.listen(port, () => {
+app.listen(port=3000, () => {
     console.log(`Example app listening on port ${port}`)
 })
---
+++ Praticando NodeEJS/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+var port = process.env.PORT || 3000
+
+const bodyParser = require('body-parser')
+const path = require('path')
+const flash = require('req-flash')
+var session = require('express-session')
+
+app.use(session({secret:'123', resave:true, saveUninitialized:true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(flash())
+
+
+
+app.set('view engine', 'ejs')
+app.set('views', '.src/viewa')
+
+app.use(express.static(path.join('src','public')))
+
+app.listen(port, ()=>{
+    console.log(`Servidor ligado na porta ${port}`)
+})
